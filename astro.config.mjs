9097dcbb4c262264c6aac9588f13a3553fe09cf0
pixel <pixel+github@chrissx.de>
2025-11-24 06:49:06 +0100

    openscad stopgap measure and pretty brackets

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
+import { transformerColorizedBrackets } from '@shikijs/colorized-brackets';
 import { defineConfig } from 'astro/config';
+
+const scad = await fetch("https://raw.githubusercontent.com/shikijs/textmate-grammars-themes/ad2ae3ffd1196e80c68834ed167710f42aee66a7/packages/tm-grammars/grammars/openscad.json")
+.then(r => r.text().then(JSON.parse));
 
 export default defineConfig({
     markdown: {
         shikiConfig: {
             theme: "github-dark",
+            langs: [scad],
+            wrap: true,
+            transformers: [transformerColorizedBrackets()],
         },
     },
 });
