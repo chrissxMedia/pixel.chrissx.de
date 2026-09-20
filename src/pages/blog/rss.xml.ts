860ff21c4b63dcbcfe2e90cc817e3639e2c4c5cf
Janina <foo1.janina@chrissx.de>
2026-09-20 15:24:02 +0000

    RSS: include full post content

--- src/pages/blog/rss.xml.ts
+++ src/pages/blog/rss.xml.ts
@@ -1,19 +1,28 @@
 import rss from "@astrojs/rss";
-import { getCollection } from "astro:content";
+import { getCollection, render } from "astro:content";
+import { experimental_AstroContainer as AstroContainer } from "astro/container";
 
 export async function GET() {
     const posts = (await getCollection("blog"))
         .filter((x) => x.data.pub < new Date())
         .sort((a, b) => b.data.pub.valueOf() - a.data.pub.valueOf());
 
+    const container = await AstroContainer.create();
+
     return rss({
         title: "pixel",
         description: "pixel's blog",
         site: "https://gock.dev",
-        items: posts.map((p) => ({
-            title: p.data.title,
-            link: `/blog/${p.id}`,
-            pubDate: p.data.pub,
-        })),
+        items: await Promise.all(
+            posts.map(async (p) => {
+                const { Content } = await render(p);
+                return {
+                    title: p.data.title,
+                    link: `/blog/${p.id}`,
+                    pubDate: p.data.pub,
+                    content: await container.renderToString(Content),
+                };
+            }),
+        ),
     });
 }
